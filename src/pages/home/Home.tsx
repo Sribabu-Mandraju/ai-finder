4f5ccb4e0772db4af81af8220e579cef0ef08fa1
sribabu <sribabumandraju@gmail.com>
2025-03-01 09:42:12 +0530

    new changes

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,14 +5,14 @@ import Roadmap from "../../components/landing/Roadmap";
 import Line from "../../components/shared/Line";
 import Footer from "../../components/landing/Footer";
 import System from "../../components/landing/System";
-import Hero from "../../components/landing/Hero";
+// import Hero from "../../components/landing/Hero";
 const Home = () => {
   return (
     <>
       <Navbar />
       <div className="mt-[70px]">
         {" "}
-        <Hero />
+        {/* <Hero /> */}
         <div className="mt-6"></div>
         <Line />
         <Products />
